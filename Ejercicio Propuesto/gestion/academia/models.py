8dc967d0cb6b17c099845d0a263225d073feb1a5
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-24 21:54:20 -0500

    creando los modelos: Alumno,Curso,NotaAlumnoPorCurso

--- Ejercicio Propuesto/gestion/academia/models.py
+++ Ejercicio Propuesto/gestion/academia/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class NotaAlumnoPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"Nota {self.nota} de {self.alumno} en {self.curso}"
