611a47925b102e99796d526a9e2307419cc1b4c5
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-24 22:00:12 -0500

    registrando los modelos en el admin

--- Ejercicio Propuesto/gestion/academia/admin.py
+++ Ejercicio Propuesto/gestion/academia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Alumno, Curso, NotaAlumnoPorCurso
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(NotaAlumnoPorCurso)
